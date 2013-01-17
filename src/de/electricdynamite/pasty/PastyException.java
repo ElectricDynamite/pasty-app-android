bbd5f720bd191b0fc2dd2f772a75f26f4a9e35b8
Philipp Geschke <pg@electricdynamite.de>
2013-01-17 11:47:32 +0100

    Updated copyright notice

--- src/de/electricdynamite/pasty/PastyException.java
+++ src/de/electricdynamite/pasty/PastyException.java
@@ -1,7 +1,7 @@
 package de.electricdynamite.pasty;
 
 /*
- *    Copyright 2012 Philipp Geschke
+ *  Copyright 2012 Philipp Geschke
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
